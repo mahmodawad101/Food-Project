e1074528a86f4707174d6903082e2c1ac919e2df
Mahmoud Awad <medo39286@gmail.com>
2022-12-12 16:31:41 +0200

    add all files
    add files

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.list').click(function () { 
+        $('.navbar').slideToggle(400);
+        
+    });
+});
+let navLinks = document.querySelectorAll(".navbar li");
+navLinks.forEach(link =>{
+    link.addEventListener('click',function(e){
+        navLinks.forEach(ele =>{
+            ele.classList.remove('lin-active')
+        });
+        e.currentTarget.classList.add('lin-active')
+    });
+});
+ScrollReveal().reveal('.box:nth-child(even)', { delay: 200 });
+ScrollReveal().reveal('.box:nth-child(odd)', { delay: 300 });
